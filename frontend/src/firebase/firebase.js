4fed8c5c1bb4ec5c47a45396a517173d4238aa81
chris <christine.rv4573@gmail.com>
2025-04-04 17:39:07 +0530

    intializiing react + firebase real time database

---
+++ frontend/src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAiCyW7iAQqeekhKo0Yb963RfOjYb8V1r0",
+    authDomain: "hacknite-anonymous-forum.firebaseapp.com",
+    projectId: "hacknite-anonymous-forum",
+    storageBucket: "hacknite-anonymous-forum.firebasestorage.app",
+    messagingSenderId: "254476899787",
+    appId: "1:254476899787:web:a0e7666b5fc42111b2d24e",
+    measurementId: "G-36T3WLE0M9",
+    databaseURL: "https://hacknite-anonymous-forum-default-rtdb.firebaseio.com"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
