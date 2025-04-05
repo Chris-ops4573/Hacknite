1f30d7878b47b9d9d72ca132f59e058e7eb9063b
chris <christine.rv4573@gmail.com>
2025-04-05 15:49:51 +0530

    made login page error handling without css

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -6,15 +6,33 @@ const SignupPage = () => {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const handleSignIn = async () => {
+        try{
+            await SignInUser(email, password)
+        } catch(error) {
+            setError(error.message)
+        }
+    }
+
+    const handleSignup = async () => {
+        try{
+            await SignupUser(email, password)
+        } catch(error) {
+            setError(error.message)
+        }
+    }
 
     return(
         <div clasName="signup-page">
             <label>Email</label>
-            <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" required placeholder="Enter your email"/>
+            <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" required placeholder="Enter your email" className="email-input"/>
             <label>Password</label>
-            <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" required placeholder="Enter your password"/>
-            <button onClick={() => SignupUser(email, password)}>Sign Up</button>
-            <button onClick={() => SignInUser(email, password)}>Sign In instead!</button>
+            <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" required placeholder="Enter your password" className="password-input"/>
+            <button onClick={() => handleSignup()} className="signup-button">Sign Up</button>
+            <button onClick={() => handleSignIn()} className="signin-button">Sign In instead!</button>
+            {error && <p className="error-message">{error}</p>}
         </div>
     )
 }
