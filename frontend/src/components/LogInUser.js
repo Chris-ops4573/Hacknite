854e28268613ecdb42ec24907763f87afb4104bb
chris <christine.rv4573@gmail.com>
2025-04-05 16:51:11 +0530

    Made sure only students of iiitb can access forum

--- frontend/src/components/LogInUser.js
+++ frontend/src/components/LogInUser.js
@@ -4,6 +4,11 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 const auth = getAuth(app)
 
 export const SignInUser = async (email, password) => {
+
+    if(!email.endsWith("@iiitb.ac.in")){
+        throw new Error("Only Students of iiitb can access this forum, if you are a student please use your college given email.")
+    }
+
     try{
         return await signInWithEmailAndPassword(auth, email, password)
     } catch(error) {
--- frontend/src/components/SignupUser.js
+++ frontend/src/components/SignupUser.js
@@ -4,6 +4,11 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 const auth = getAuth(app)
 
 export const SignupUser = async (email, password) => {
+
+    if(!email.endsWith("@iiitb.ac.in")){
+        throw new Error("Only Students of iiitb can access this forum, if you are a student please use your college given email.")
+    }    
+
     try {
         return await createUserWithEmailAndPassword(auth, email, password)
     } catch(error) {
