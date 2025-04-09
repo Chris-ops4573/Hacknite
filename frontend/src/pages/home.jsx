728c391dc70b1ad9be6cc6342f66829510e0da1c
chris <christine.rv4573@gmail.com>
2025-04-09 15:15:48 +0530

    added components to render posts in homepage and display in real time

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { onValue, ref, getDatabase } from "firebase/database";
+import formatDistanceToNow from "date-fns/formatDistanceToNow"
 
 const Home = () => {
      
     const [posts, setPosts] = useState([])
     const db = getDatabase()
-    const postRef = ref(db, 'posts')
+    const postRef = ref(db, 'post')
     
     useEffect(() => {
         const stopUpdating = onValue(postRef , (snapshot) => {
@@ -25,6 +26,12 @@ const Home = () => {
     return(
         <div>
             <h1>Home page</h1>
+            {posts.map((post) => (
+                <div key={post.id} className="post">
+                    <p>{post.content}</p>
+                    {post.createdAt ? <p>{formatDistanceToNow(new Date(post.createdAt))}</p> : <p>Time not available</p>}
+                </div>
+            ))}
         </div>
     )
 }
