c3b9f981049cc16fe998d53d3d9f80e7091425cc
chris <christine.rv4573@gmail.com>
2025-04-13 00:33:22 +0530

    cannot add duplicate tags to posts and moved the post button

--- frontend/src/pages/postForm.jsx
+++ frontend/src/pages/postForm.jsx
@@ -7,28 +7,18 @@ const PostForm = () => {
     const [error, setError] = useState("")
     const [tags, setTags] = useState([])
 
+    const addTag = (tag) => {
+        if(!tags.includes(tag)){
+            setTags([...tags, tag])
+        } 
+    }
+
     return(
         <div className="post-form">
             <h1>Make a post</h1>
             <label>Content: </label>
             <input onChange={(e) => setContent(e.target.value)} value={content} required placeholder="Make an anonymous post" className="content-input" />
-            <h5>Add tags</h5>
-            <button onClick={() => setTags([...tags, "General "])}>General</button>
-            <button onClick={() => setTags([...tags, "Rant "])}>Rant</button>
-            <button onClick={() => setTags([...tags, "WiFi "])}>WiFi</button>
-            <button onClick={() => setTags([...tags, "Services "])}>Services</button>
-            <button onClick={() => setTags([...tags, "Clubs "])}>Clubs</button>
-            <button onClick={() => setTags([...tags, "Social "])}>Social</button>
-            <button onClick={() => setTags([...tags, "Confession "])}>Confession</button>
-            <button onClick={() => setTags([...tags, "Food "])}>Food</button>
-            <button onClick={() => setTags([...tags, "Academics "])}>Academics</button>
-            <button onClick={() => setTags([...tags, "Networking "])}>Networking</button>
-            <button onClick={() => setTags([...tags, "Cleaning "])}>Cleaning</button>
-            <button onClick={() => setTags([...tags, "Hostel "])}>Hostel</button>
-            <button onClick={() => setTags([...tags, "Going-out "])}>Going out</button>
-            <button onClick={() => setTags([...tags, "Rules "])}>Rules</button>
-            <button onClick={() => setTags([...tags, "Regarding-this-forum "])}>Regarding this forum</button>
-            <button onClick={() => {
+            <button className="post-button" onClick={() => {
                 if(content){
                     WriteAndUpdatePost(content, tags)
                     setContent("")
@@ -38,6 +28,22 @@ const PostForm = () => {
                     setError("Cannot leave an empty post")
                 }
             }}>Post</button>
+            <h5>Add tags</h5>
+            <button onClick={() => addTag("General ")}>General</button>
+            <button onClick={() => addTag("Rant ")}>Rant</button>
+            <button onClick={() => addTag("WiFi ")}>WiFi</button>
+            <button onClick={() => addTag("Services ")}>Services</button>
+            <button onClick={() => addTag("Clubs ")}>Clubs</button>
+            <button onClick={() => addTag("Social ")}>Social</button>
+            <button onClick={() => addTag("Confession ")}>Confession</button>
+            <button onClick={() => addTag("Food ")}>Food</button>
+            <button onClick={() => addTag("Academics ")}>Academics</button>
+            <button onClick={() => addTag("Networking ")}>Networking</button>
+            <button onClick={() => addTag("Cleaning ")}>Cleaning</button>
+            <button onClick={() => addTag("Hostel ")}>Hostel</button>
+            <button onClick={() => addTag("Going-out ")}>Going out</button>
+            <button onClick={() => addTag("Rules ")}>Rules</button>
+            <button onClick={() => addTag("Regarding-this-forum ")}>Regarding this forum</button>
             <div className="post-error">{error}</div>
             <p>Note: All posts are completely anonymous and cannot be traced back to any individual. However, please use this platform responsibly. Any content that violates community guidelines or our Terms of Service will be removed.</p>
         </div>
